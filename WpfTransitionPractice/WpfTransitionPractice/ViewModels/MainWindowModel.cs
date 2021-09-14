803788cdf7b496076f1a02c7a38745340ce601fe
むずでょ <muzudho1@gmail.com>
2021-09-15 00:08:51 +0900

    ViewModelCommand

--- WpfTransitionPractice/WpfTransitionPractice/ViewModels/MainWindowModel.cs
+++ WpfTransitionPractice/WpfTransitionPractice/ViewModels/MainWindowModel.cs
@@ -1,9 +1,50 @@
 ﻿namespace WpfTransitionPractice.ViewModels
 {
     using Livet;
+    using Livet.Commands;
+    using System.Windows.Input;
 
     public class MainWindowModel : ViewModel
     {
+        /*
+        /// <summary>
+        /// ユーザーのアクションと、その処理を結びつけるもの
+        /// </summary>
+        public static RoutedCommand OpeningAlice = new RoutedCommand();
 
+        private void OpeningAlice_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var window = new AliceWindow();
+            window.Show();
+        }
+
+        private void OpeningAlice_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        */
+
+        /// <summary>
+        /// https://unity-memo.hatenablog.com/entry/2015/01/24/090328
+        /// </summary>
+        private ViewModelCommand _openingAlice;
+
+        public ViewModelCommand OpeningAlice
+        {
+            get
+            {
+                if (_openingAlice == null)
+                {
+                    _openingAlice = new ViewModelCommand(OpenAlice);
+                }
+                return _openingAlice;
+            }
+        }
+
+        public void OpenAlice()
+        {
+            var window = new AliceWindow();
+            window.Show();
+        }
     }
 }
---
+++ WpfTransitionPractice/WpfTransitionPractice/Views/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace WpfTransitionPractice
+{
+    using Livet.Commands;
+    using System.Windows;
+    using System.Windows.Input;
+    using WpfTransitionPractice.ViewModels;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // public ICommand OpeningAlice { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // this.OpeningAlice = new ViewModelCommand(() => ((MainWindowModel)this.DataContext).OpenAlice());
+        }
+    }
+}
